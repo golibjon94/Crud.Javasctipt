f357ea6d1db1c7ba52020c3f98929ba34e3c0858
golibjon94 <golib94jon@gmail.com>
2020-12-30 21:07:32 +0500

    all errors have been corrected

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ function onFormSubmit() {
 function readFormData(){
         var formData = {};
         formData["fullName"] = document.getElementById("fullName").value;
-        formData["empCode"] = document.getElementById("empCode").value;
+        formData["empcode"] = document.getElementById("empcode").value;
         formData["salary"] = document.getElementById("salary").value;
         formData["city"] = document.getElementById("city").value;
         return formData;
@@ -25,7 +25,7 @@ function insertNewRecord(data) {
         cell1 = newRow.insertCell(0);
         cell1.innerHTML = data.fullName;
         cell2 = newRow.insertCell(1);
-        cell2.innerHTML = data.empCode;
+        cell2.innerHTML = data.empcode;
         cell3 = newRow.insertCell(2);
         cell3.innerHTML = data.salary;
         cell4 = newRow.insertCell(3);
